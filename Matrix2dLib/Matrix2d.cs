1ddd4d33ea10c2e1853af8b26a82d005033633aa
Jakub Zydroń <jakub.zydron@microsoft.wsei.edu.pl>
2025-04-19 11:49:23 +0200

    Add methods and tests

--- Matrix2dLib/Matrix2d.cs
+++ Matrix2dLib/Matrix2d.cs
@@ -4,35 +4,69 @@ namespace Matrix2dLib;
 // immutable 2x2 matrix
 public class Matrix2d : IEquatable<Matrix2d>
 {
-    private int _a, _b, _c, _d;
+    private int a, b, c, d;
     public Matrix2d(int a, int b, int c, int d)
     {
-        _a = a;
-        _b = b;
-        _c = c;
-        _d = d;
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
     }
     public Matrix2d() : this(1, 0, 0, 1) { }
 
     // properties (metody z dużej litery)
-    public int A => _a;
-    public int B => _b;
-    public int C => _c;
-    public int D => _d;
+    public int A => a;
+    public int B => b;
+    public int C => c;
+    public int D => d;
     public static Matrix2d Id => new Matrix2d();
     public static Matrix2d Zero => new Matrix2d(0, 0, 0, 0);
 
-    // methods
-    override public string ToString() => $"[[{_a}, {_b}], [{_c}, {_d}]]";
+    // metody
+    override public string ToString() => $"[[{a}, {b}], [{c}, {d}]]";
+
+    public static Matrix2d Parse(string matrix)
+    {
+        if (string.IsNullOrWhiteSpace(matrix))
+            throw new FormatException("Input string is null or empty.");
+
+        // Sprawdź, czy format pasuje do oczekiwanego wzorca
+        if (!matrix.StartsWith("[[") || !matrix.EndsWith("]]"))
+            throw new FormatException("Input string must start with '[[' and end with ']]'.");
+
+        // Usuń zewnętrzne nawiasy
+        matrix = matrix.Substring(2, matrix.Length - 4);
+
+        // Podziel na wiersze
+        var rows = matrix.Split("], [");
+        if (rows.Length != 2)
+            throw new FormatException("Input string must contain exactly two rows.");
+
+        try
+        {
+            // Parsuj elementy wierszy
+            var row1 = rows[0].Split(',').Select(int.Parse).ToArray();
+            var row2 = rows[1].Split(',').Select(int.Parse).ToArray();
+
+            if (row1.Length != 2 || row2.Length != 2)
+                throw new FormatException("Each row must contain exactly two elements.");
+
+            return new Matrix2d(row1[0], row1[1], row2[0], row2[1]);
+        }
+        catch (Exception ex)
+        {
+            throw new FormatException("Input string is not in the correct format.");
+        }
+    }
 
     #region Equals
     public bool Equals(Matrix2d other)
     {
         if (other is null) return false;
-        return _a == other._a 
-            && _b == other._b 
-            && _c == other._c 
-            && _d == other._d;
+        return a == other.a
+            && b == other.b 
+            && c == other.c 
+            && d == other.d;
     }
 
     public static bool operator ==(Matrix2d left, Matrix2d right)
@@ -44,45 +78,61 @@ public class Matrix2d : IEquatable<Matrix2d>
     #region Artmetic
     public static Matrix2d operator +(Matrix2d left, Matrix2d right)
         => new Matrix2d(
-            left._a + right._a, 
-            left._b + right._b, 
-            left._c + right._c, 
-            left._d + right._d
+            left.a + right.a, 
+            left.b + right.b, 
+            left.c + right.c, 
+            left.d + right.d
             );
     public static Matrix2d operator -(Matrix2d left, Matrix2d right)
         => new Matrix2d(
-            left._a - right._a,
-            left._b - right._b,
-            left._c - right._c,
-            left._d - right._d
+            left.a - right.a,
+            left.b - right.b,
+            left.c - right.c,
+            left.d - right.d
             );
     public static Matrix2d operator *(Matrix2d left, Matrix2d right)
         => new Matrix2d(
-            left._a * right._a + left._b * right._c,
-            left._a * right._b + left._b * right._d,
-            left._c * right._a + left._d * right._c,
-            left._c * right._b + left._d * right._d
+            left.a * right.a + left.b * right.c,
+            left.a * right.b + left.b * right.d,
+            left.c * right.a + left.d * right.c,
+            left.c * right.b + left.d * right.d
             );
     public static Matrix2d operator *(Matrix2d left, int right)
         => new Matrix2d(
-            left._a * right,
-            left._b * right,
-            left._c * right,
-            left._d * right
+            left.a * right,
+            left.b * right,
+            left.c * right,
+            left.d * right
             );
     public static Matrix2d operator *(int left, Matrix2d right)
         => right * left;
 
     public static Matrix2d operator -(Matrix2d m)
-        => new Matrix2d(-m._a, -m._b, -m._c, -m._d);
+        => new Matrix2d(-m.a, -m.b, -m.c, -m.d);
 
-    public Matrix2d Transpose()
-        => new Matrix2d(_a, _c, _b, _d);
+    public Matrix2d Transpose(Matrix2d A)
+    {
+        if (A == null)
+            throw new ArgumentNullException();
+        return new Matrix2d(A.a, A.c, A.b, A.d);
+    }
+
+    public int Det()
+    {
+        return a * d - b * c;
+    }
+
+    public int Determinant(Matrix2d A)
+    {
+        if (A == null)
+            throw new ArgumentNullException();
+        return A.a * A.d - A.b * A.c;
+    }
     #endregion
     #region Conversion
     // konwersja jawna z Matrix2d do int[,]
     public static explicit operator int[,](Matrix2d m)
-        => new int[,] { { m._a, m._b }, { m._c, m._d } };
+        => new int[,] { { m.a, m.b }, { m.c, m.d } };
 
     // konwersja niejawna z int[,] do Matrix2d
     public static implicit operator Matrix2d(int[,] m)
--- Matrix2dTest/Test1.cs
+++ Matrix2dTest/Test1.cs
@@ -6,17 +6,24 @@ namespace Matrix2dTest
     public sealed class Test1
     {
         [TestMethod]
-        public void Konstruktor_WieleArgumentow_PoprawneDane_Ok()
+        [DataRow(1, 2, 3, 4)]
+        [DataRow(0, 0, 0, 0)]
+        [DataRow(-1, -2, -3, -4)]
+        [DataRow(123, 456, 789, 1011)]
+        [DataRow(10, 20, 30, 40)]
+        public void Konstruktor_WieleArgumentow_PoprawneDane_Ok(int a, int b, int c, int d)
         {
             // Arrange + Act
-            var m = new Matrix2d(1, 2, 3, 4);
+            var m = new Matrix2d(a, b, c, d);
 
             // Assert
-            Assert.AreEqual(1, m.A);
-            Assert.AreEqual(2, m.B);
-            Assert.AreEqual(3, m.C);
-            Assert.AreEqual(4, m.D);
+            Assert.AreEqual(a, m.A);
+            Assert.AreEqual(b, m.B);
+            Assert.AreEqual(c, m.C);
+            Assert.AreEqual(d, m.D);
         }
+
+
         [TestMethod]
         public void Konstruktor_BrakArgumentow_PoprawneDane_Ok()
         {
@@ -27,6 +34,197 @@ namespace Matrix2dTest
             Assert.AreEqual(0, m.B);
             Assert.AreEqual(0, m.C);
             Assert.AreEqual(1, m.D);
+        }
+
+        [TestMethod]
+        [DataRow(1, 2, 3, 4, "[[1, 2], [3, 4]]")]
+        [DataRow(0, 0, 0, 0, "[[0, 0], [0, 0]]")]
+        [DataRow(-1, -2, -3, -4, "[[-1, -2], [-3, -4]]")]
+        [DataRow(123, 456, 789, 1011, "[[123, 456], [789, 1011]]")]
+        [DataRow(10, 20, 30, 40, "[[10, 20], [30, 40]]")]
+        public void ToString_PrawidlowyFormat_Ok(int a, int b, int c, int d, string expected)
+        {
+            // Arrange
+            var m = new Matrix2d(a, b, c, d);
+
+            // Act
+            var result = m.ToString();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [DataRow("[[1, 12], [3, 444]]", 1, 12, 3, 444)]
+        [DataRow("[[0, 0], [0, 0]]", 0, 0, 0, 0)]
+        [DataRow("[[-1, -2], [-3, -4]]", -1, -2, -3, -4)]
+        [DataRow("[[123, 456], [789, 1011]]", 123, 456, 789, 1011)]
+        [DataRow("[[10, 20], [30, 40]]", 10, 20, 30, 40)]
+        public void Parse_PrawidlowyFormat_Ok(string input, int expectedA, int expectedB, int expectedC, int expectedD)
+        {
+            // Act
+            var result = Matrix2d.Parse(input);
+
+            // Assert
+            Assert.AreEqual(expectedA, result.A);
+            Assert.AreEqual(expectedB, result.B);
+            Assert.AreEqual(expectedC, result.C);
+            Assert.AreEqual(expectedD, result.D);
+        }
+
+        [TestMethod]
+        [DataRow(1, 2, 3, 4, 1, 2, 3, 4)]
+        [DataRow(0, 0, 0, 0, 0, 0, 0, 0)]
+        [DataRow(-1, -2, -3, -4, -1, -2, -3, -4)]
+        [DataRow(123, 456, 789, 1011, 123, 456, 789, 1011)]
+        [DataRow(10, 20, 30, 40, 10, 20, 30, 40)]
+        public void Equals_IdentyczneMacierze_Ok(int a, int b, int c, int d, int e, int f, int g, int h)
+        {
+            // Arrange
+            var m1 = new Matrix2d(a, b, c, d);
+            var m2 = new Matrix2d(e, f, g, h);
+            // Act
+            var result = m1.Equals(m2);
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        [DataRow(1, 2, 3, 4, 1, 2, 3, 5)]
+        [DataRow(0, 0, 0, 0, 0, 0, 1, 0)]
+        [DataRow(-1, -2, -3, -4, -1, -2, -3, 4)]
+        [DataRow(123, 456, 789, 1011, 123, 456, 789, 1012)]
+        [DataRow(10, 20, 30, 40, 10, 20, 30, 50)]
+        public void Equals_RozneMacierze_Ok(int a, int b, int c, int d, int e, int f, int g, int h)
+        {
+            // Arrange
+            var m1 = new Matrix2d(a, b, c, d);
+            var m2 = new Matrix2d(e, f, g, h);
+            // Act
+            var result = m1.Equals(m2);
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Id_Property_Ok()
+        {
+            // Act
+            var identityMatrix = Matrix2d.Id;
+
+            // Assert
+            Assert.AreEqual(1, identityMatrix.A);
+            Assert.AreEqual(0, identityMatrix.B);
+            Assert.AreEqual(0, identityMatrix.C);
+            Assert.AreEqual(1, identityMatrix.D);
+        }
+
+        [TestMethod]
+        public void Zero_Property_ZeroMatrix_Ok()
+        {
+            // Act
+            var zeroMatrix = Matrix2d.Zero;
+
+            // Assert
+            Assert.AreEqual(0, zeroMatrix.A);
+            Assert.AreEqual(0, zeroMatrix.B);
+            Assert.AreEqual(0, zeroMatrix.C);
+            Assert.AreEqual(0, zeroMatrix.D);
+        }
+
+        [TestMethod]
+        public void Equals_Null_Falsz()
+        {
+            // Arrange
+            var matrix = new Matrix2d(1, 2, 3, 4);
+
+            // Act
+            var result = matrix.Equals(null);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void OperatorRownosci_DwieTakieSameMacierze_ZwracaTrue()
+        {
+            // Arrange
+            var m1 = new Matrix2d(1, 2, 3, 4);
+            var m2 = new Matrix2d(1, 2, 3, 4);
+
+            // Act
+            var result = m1 == m2;
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void OperatorRownosci_DwieRozneMacierze_ZwracaFalse()
+        {
+            // Arrange
+            var m1 = new Matrix2d(1, 2, 3, 4);
+            var m2 = new Matrix2d(5, 6, 7, 8);
+
+            // Act
+            var result = m1 == m2;
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void OperatorRownosci_PorownanieZNull_ZwracaFalse()
+        {
+            // Arrange
+            var m1 = new Matrix2d(1, 2, 3, 4);
+
+            // Act
+            var result = m1 == null;
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void OperatorNierownosci_DwieRozneMacierze_ZwracaTrue()
+        {
+            // Arrange
+            var m1 = new Matrix2d(1, 2, 3, 4);
+            var m2 = new Matrix2d(5, 6, 7, 8);
+
+            // Act
+            var result = m1 != m2;
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void OperatorNierownosci_DwieTakieSameMacierze_ZwracaFalse()
+        {
+            // Arrange
+            var m1 = new Matrix2d(1, 2, 3, 4);
+            var m2 = new Matrix2d(1, 2, 3, 4);
+
+            // Act
+            var result = m1 != m2;
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void OperatorNierownosci_PorownanieZNull_ZwracaTrue()
+        {
+            // Arrange
+            var m1 = new Matrix2d(1, 2, 3, 4);
+
+            // Act
+            var result = m1 != null;
+
+            // Assert
+            Assert.IsTrue(result);
         }
     }
 }
